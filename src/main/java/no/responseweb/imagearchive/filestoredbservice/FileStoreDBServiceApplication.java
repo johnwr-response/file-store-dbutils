cb836b7c86ace794c1139f9e91780459818b3dbe
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-06-21 23:33:55 +0200

    Initial commit

---
+++ src/main/java/no/responseweb/imagearchive/filestoredbservice/FileStoreDBServiceApplication.java
@@ -0,0 +1,13 @@
+package no.responseweb.imagearchive.filestoredbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStoreDBServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStoreDBServiceApplication.class, args);
+	}
+
+}
